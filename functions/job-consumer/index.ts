043b2062accc5d88a09165f59acba4bf1a77db6e
jkeys <jkeys@ecg-hq.com>
2021-03-03 01:08:16 -0700

    wrap both functions in try/catch to prevent queue<->lambda loops

--- functions/job-consumer/index.ts
+++ functions/job-consumer/index.ts
@@ -6,34 +6,38 @@ const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1' });
 
 exports.handler = async (event : SQSEvent) => {
     for(const record of event.Records) {
-        const { messageId, body} = record;
-
-        const parsed = JSON.parse(body) as Job;
-        
-        const { url } = parsed;
-
-        const response = await axios.get(url);
-        const { data } = response;
-
-        if(!data) {
-            console.error('something went wrong, reprocess this (TODO)');
-            continue;
-        }
-
-        console.log('data: ', data);
-
-        await docClient.put({
-            TableName: 'active-jobs',
-            Item: {
-                ...parsed,
-                updatedAt: new Date().toISOString(),
-                response: data,
-                status: 'Ready'
+        try {
+            const { messageId, body} = record;
+    
+            const parsed = JSON.parse(body) as Job;
+            
+            const { url } = parsed;
+    
+            const response = await axios.get(url);
+            const { data } = response;
+    
+            if(!data) {
+                console.error('something went wrong, reprocess this (TODO)');
+                continue;
             }
-        }).promise().catch(e => {
-            console.error('something went wrong updating DynamoDB, reprocess this (TODO)')
-        });
-    };
+    
+            console.log('data: ', data);
+    
+            await docClient.put({
+                TableName: 'active-jobs',
+                Item: {
+                    ...parsed,
+                    updatedAt: new Date().toISOString(),
+                    response: data,
+                    status: 'Ready'
+                }
+            }).promise().catch(e => {
+                console.error('something went wrong updating DynamoDB, reprocess this (TODO)')
+            });
+        } catch(e) {
+            console.error(`error: `, e);
+        }
+    }
 
     return 'success';
 };
--- functions/job-queue/index.ts
+++ functions/job-queue/index.ts
@@ -28,74 +28,79 @@ const ApplicationConstants = {
 }
 
 exports.handler = async (event : APIGatewayProxyEvent) => {
-    console.log('event: ', JSON.stringify(event));
-    const { body, path, httpMethod, queryStringParameters } = event;
-
-    if(!queryStringParameters || !queryStringParameters.url) {
-        return buildResponse(400, { errorMessage: 'malformed request, please send the URL to fetch as an encoded query parameter: url=myurl.com'})
-    }
-
-    const { url } = queryStringParameters;
-
-    //handle retrieving the results, or return 202 Accepted or 404 Not Found
-    if(httpMethod === 'GET') {
-        //lookup in DynamoDB
-        const { Item } = await docClient.get({
-            TableName: ApplicationConstants.tableName,
-            Key: {
-                url
+    try {
+        console.log('event: ', JSON.stringify(event));
+        const { body, path, httpMethod, queryStringParameters } = event;
+    
+        if(!queryStringParameters || !queryStringParameters.url) {
+            return buildResponse(400, { errorMessage: 'malformed request, please send the URL to fetch as an encoded query parameter: url=myurl.com'})
+        }
+    
+        const { url } = queryStringParameters;
+    
+        //handle retrieving the results, or return 202 Accepted or 404 Not Found
+        if(httpMethod === 'GET') {
+            //lookup in DynamoDB
+            const { Item } = await docClient.get({
+                TableName: ApplicationConstants.tableName,
+                Key: {
+                    url
+                }
+            }).promise();
+    
+            //if job.status === Ready, then return job.response 
+            if(Item) {
+                return buildResponse(404, Item.response);
+            } else {
+                return buildResponse(404, { errorMessage: 'resource not found'});
             }
-        }).promise();
-
-        //if job.status === Ready, then return job.response 
-        if(Item) {
-            return buildResponse(404, Item.response);
+        } else if(httpMethod === 'POST') {
+            const now = moment();
+            const newJob: Job = {
+                url,
+                status: 'Accepted',
+                createdAt: now.toISOString(),
+                updatedAt: now.toISOString(),
+            }
+    
+            //lookup in DynamoDB
+            const { Item } = await docClient.get({
+                TableName: ApplicationConstants.tableName,
+                Key: {
+                    url
+                }
+            }).promise();
+    
+            //if it exists, check if it has been requested in the last hour
+            if(Item) {
+                const job = Item as Job;
+                const { status, createdAt } = job;
+                const created = moment(createdAt);
+                const expired = moment(createdAt).add(1, 'h');
+    
+                if(now.isBetween(created, expired)) {
+                    return buildResponse(200, { message: 'Request created within the last hour (skipped fetch)'});
+                }
+    
+                if(status === 'Processing') {
+                    return buildResponse(202, { message: 'still processing. Grab some coffee!' });
+                }
+            } 
+    
+            await docClient.put({ 
+                TableName: ApplicationConstants.tableName, 
+                Item: newJob 
+            }).promise();
+    
+            //send to SQS
+            await startJob(newJob);
+    
+            return buildResponse(202, { message: 'Accepted'});
         } else {
-            return buildResponse(404, { errorMessage: 'resource not found'});
-        }
-    } else if(httpMethod === 'POST') {
-        const now = moment();
-        const newJob: Job = {
-            url,
-            status: 'Accepted',
-            createdAt: now.toISOString(),
-            updatedAt: now.toISOString(),
-        }
-
-        //lookup in DynamoDB
-        const { Item } = await docClient.get({
-            TableName: ApplicationConstants.tableName,
-            Key: {
-                url
-            }
-        }).promise();
-
-        //if it exists, check if it has been requested in the last hour
-        if(Item) {
-            const job = Item as Job;
-            const { status, createdAt } = job;
-            const created = moment(createdAt);
-            const expired = moment(createdAt).add(1, 'h');
-
-            if(now.isBetween(created, expired)) {
-                return buildResponse(200, { message: 'Request created within the last hour (skipped fetch)'});
-            }
-
-            if(status === 'Processing') {
-                return buildResponse(202, { message: 'still processing. Grab some coffee!' });
-            }
-        } 
-
-        await docClient.put({ 
-            TableName: ApplicationConstants.tableName, 
-            Item: newJob 
-        }).promise();
-
-        //send to SQS
-        await startJob(newJob);
-
-        return buildResponse(202, { message: 'Accepted'});
-    } else {
-        return buildResponse(400, { errorMessage: 'this endpoint only accepts GET and POST'});
-    };
+            return buildResponse(400, { errorMessage: 'this endpoint only accepts GET and POST'});
+        };
+    } catch(e) {
+        console.log("error: ", e);
+        return buildResponse(400, { errorMessage: 'something went wrong'});
+    }
 };
