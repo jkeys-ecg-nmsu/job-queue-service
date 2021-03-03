8d9dc0a7e08333499423e2d16131d824382f1e19
jkeys <jkeys@ecg-hq.com>
2021-03-03 10:45:25 -0700

    set status=Processing when consumer grabs the job from the queue

--- functions/job-consumer/index.ts
+++ functions/job-consumer/index.ts
@@ -12,6 +12,21 @@ exports.handler = async (event : SQSEvent) => {
             const parsed = JSON.parse(body) as Job;
             
             const { url } = parsed;
+
+            //set the status of the request to Processing
+            await docClient.update({
+                TableName: 'active-jobs',
+                Key: {
+                    url
+                },
+                UpdateExpression: 'SET #status = :proc',
+                ExpressionAttributeNames: {
+                    '#status' : 'status'
+                },
+                ExpressionAttributeValues: {
+                    ':proc' : 'Processing'
+                }
+            }).promise();
     
             const response = await axios.get(url);
             const { data } = response;
